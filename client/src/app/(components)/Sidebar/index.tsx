72a620ac73b7f6ed2a4900edf7851d81e03ec97a
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-08-08 22:46:13 +0530

    UI COMMIT

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,9 +1,31 @@
+"use client";
+import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      {/* TOP LOGO */}
+      <div className="flex justify-between md:justify-normal items-center pt-8 gap-3">
+        <div>logo</div>
+        <h1 className="font font-extrabold text-2xl">STOCKEASE</h1>
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}>
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+      {/* LINKS */}
+      <div className="flex-grow mt-8">
+
+      </div>
+
+      {/* FOOTER */}
+      <div>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 stockease</p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
