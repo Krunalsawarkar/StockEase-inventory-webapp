74797821c13f71592de059a5e393de81c0a101b2
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-08-13 22:11:49 +0530

    Frontend Commit

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,28 +1,135 @@
 "use client";
-import { Menu } from "lucide-react";
+import {
+  Archive,
+  CircleDollarSign,
+  Clipboard,
+  Layout,
+  LucideIcon,
+  Menu,
+  SlidersHorizontal,
+  User,
+} from "lucide-react";
 import React from "react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+interface SidebarlinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
+}: SidebarlinkProps) => {
+  const path = usePathname();
+  const Active = path === href || (path === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href}>
+      <div
+        className={`cursor-pointer flex items-center ${
+          isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
+        } hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
+          Active ? "bg-blue-200 text-white" : ""
+        }`}>
+        <Icon className="w-6 h-6 !text-gray-700" />
+        <span
+          className={`${
+            isCollapsed ? "hidden" : "block"
+          } font-medium text-gray-700`}>
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSideBarCollapsed));
+  };
+
+  const sidebarClasses = `fixed flex flex-col ${
+    isSideBarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
   return (
-    <div>
+    <div className={sidebarClasses}>
       {/* TOP LOGO */}
-      <div className="flex justify-between md:justify-normal items-center pt-8 gap-3">
+      <div
+        className={`flex justify-between md:justify-normal items-center pt-8 gap-3  ${
+          isSideBarCollapsed ? "px-5" : "px-8"
+        }`}>
         <div>logo</div>
-        <h1 className="font font-extrabold text-2xl">STOCKEASE</h1>
+        <h1
+          className={`font font-extrabold text-2xl ${
+            isSideBarCollapsed ? "hidden" : "block"
+          }`}>
+          STOCKEASE
+        </h1>
         <button
           className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}>
+          onClick={toggleSidebar}>
           <Menu className="w-4 h-4" />
         </button>
       </div>
       {/* LINKS */}
       <div className="flex-grow mt-8">
-
+        <SidebarLink
+          href="/dashboard"
+          icon={Layout}
+          label="Dashboard"
+          isCollapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/inventory"
+          icon={Archive}
+          label="Inventory"
+          isCollapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/products"
+          icon={Clipboard}
+          label="Products"
+          isCollapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/users"
+          icon={User}
+          label="User"
+          isCollapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/settings"
+          icon={SlidersHorizontal}
+          label="Settings"
+          isCollapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/expenses"
+          icon={CircleDollarSign}
+          label="Expenses"
+          isCollapsed={isSideBarCollapsed}
+        />
       </div>
 
       {/* FOOTER */}
-      <div>
-        <p className="text-center text-xs text-gray-500">&copy; 2024 stockease</p>
+      <div className={`${isSideBarCollapsed ? "hidden" : "block"} mb-10`}>
+        <p className="text-center text-xs text-gray-500">
+          &copy; 2024 stockease
+        </p>
       </div>
     </div>
   );
